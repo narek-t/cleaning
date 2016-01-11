2064e484e9d64c78eeb2a9e7cb47922482a569c9
Narek-T <narek.tarverdyan@bk.ru>
2016-01-11 18:19:40 +0400

    11

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,25 @@ var jade = require('gulp-jade');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
+var del = require('del');
+
+//del removed or renamed files
+var delDest;
+gulp.task('del', function (cb) {
+ return del(delDest,cb);
+ });
+
 //sass
 gulp.task('sass', function() {
+	delDest = 'dist/css';
 	return gulp.src('src/scss/**/*.scss')
 		.pipe(plumber())
 		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 5 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('dist/css'))
 		.pipe(rename({
 			suffix: '.min'
 		}))
@@ -36,7 +50,8 @@ gulp.task('browserSync', function() {
 });
 //jade
 gulp.task('jade', function() {
-	return gulp.src('src/templates/**/*.jade')
+	delDest = 'dist/*.html';
+	return gulp.src('src/templates/**/!(_)*.jade')
 		.pipe(plumber())
 		.pipe(jade({
 			pretty: true
@@ -48,8 +63,10 @@ gulp.task('jade', function() {
 });
 //scripts
 gulp.task('uglify', function() {
+	delDest = 'dist/js/*.js';
 	return gulp.src('src/scripts/**/*.js')
 		.pipe(plumber())
+		.pipe(gulp.dest('dist/js'))
 		.pipe(concat('app.min.js'))
 		.pipe(gulp.dest('dist/js'))
 		.pipe(uglify({
@@ -62,7 +79,7 @@ gulp.task('uglify', function() {
 });
 
 gulp.task('watch', ['browserSync', 'sass', 'jade', 'uglify'], function() {
-	gulp.watch('src/scss/**/*.scss', ['sass']);
-	gulp.watch('src/templates/**/*.jade', ['jade']);
-	gulp.watch('src/scripts/**/*.js', ['uglify']);
+	gulp.watch('src/scss/**/*.scss', ['del', 'sass']);
+	gulp.watch('src/templates/**/*.jade', ['del', 'jade']);
+	gulp.watch('src/scripts/**/*.js', ['del', 'uglify']);
 });
