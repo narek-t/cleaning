606d9ba0fbd24746e6475561d391c82fa8a34392
Narek-T <narek.tarverdyan@bk.ru>
2016-01-16 14:01:47 +0400

    cleaning frontend

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
-	console.log("hello")
+	$('.bxslider').bxSlider({
+		pager: false,
+	});
+	$('.topslider').bxSlider({
+		// mode: 'fade',
+		// speed: 1500
+	});
+	$('.fancybox').fancybox({
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
+	$("li.has-sub > a").click(function() {
+		$(this).toggleClass('closed');
+		var ul = $(this).next(),
+			clone = ul.clone().css({
+				"height": "auto"
+			}).appendTo(".mini-menu"),
+			height = ul.css("height") === "0px" ? ul[0].scrollHeight + "px" : "0px";
+		clone.remove();
+		ul.animate({
+			"height": height
+		});
+		return false;
+	});
 });
